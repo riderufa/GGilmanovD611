1c63c3040353a9ba1b2e667eec8f38d8debe1928
Gilmanov Garri <3434455@mail.ru>
2019-12-19 22:54:30 +0500

    deploy3

--- my_site/settings.py
+++ my_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pure-mountain-45134.herokuapp.com']
+ALLOWED_HOSTS = ['fast-reaches-54521.herokuapp.com']
 # ALLOWED_HOSTS = []
 
 
